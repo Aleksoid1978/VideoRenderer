10c3254cbaf653e3e702d88f4e1db7b1701206f2
v0lt <mpc-be@mail.ru>
2019-07-01 18:58:58 +0300

    DX11: Добавлен шейдер ps_convert_nv12.hlsl.

---
+++ Source/Shaders/d3d11/ps_convert_nv12.hlsl
@@ -0,0 +1,53 @@
+#ifndef C_HDR
+    #define C_HDR 0
+#endif
+
+Texture2D texY : register(t0);
+Texture2D texUV : register(t1);
+SamplerState samp : register(s0);
+
+cbuffer PS_COLOR_TRANSFORM : register(b0)
+{
+    float3 cm_r;
+    float3 cm_g;
+    float3 cm_b;
+    float3 cm_c;
+    // NB: sizeof(float3) == sizeof(float4)
+};
+cbuffer PS_TEX_DIMENSIONS : register(b4)
+{
+    float width;
+    float height;
+    float dx;
+    float dy;
+};
+
+#if (C_HDR == 1)
+#include "../convert/correct_st2084.hlsl"
+#elif (C_HDR == 2)
+#include "../convert/correct_hlg.hlsl"
+#endif
+
+struct PS_INPUT
+{
+    float4 Pos : SV_POSITION;
+    float2 Tex : TEXCOORD;
+};
+
+float4 main(PS_INPUT input) : SV_Target
+{
+    float colorY = texY.Sample(samp, input.Tex).r;
+    float2 colorUV = texUV.Sample(samp, input.Tex).rg;
+
+    float4 color = float4(colorY, colorUV, 0);
+
+    color.rgb = float3(mul(cm_r, color.rgb), mul(cm_g, color.rgb), mul(cm_b, color.rgb)) + cm_c;
+
+#if (C_HDR == 1)
+    color = correct_ST2084(color);
+#elif (C_HDR == 2)
+    color = correct_HLG(color);
+#endif
+
+    return color;
+}
