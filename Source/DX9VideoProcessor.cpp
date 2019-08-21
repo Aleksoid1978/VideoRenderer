24fc4f3f6e0e1fb71c6acad1fcadd0d0bcdf2236
Aleksoid1978 <Aleksoid1978@mail.ru>
2019-08-22 09:13:06 +1000

    Изменение : DX9 - исправлено получение текущего кадра(IBasicVideo::GetCurrentImage()) при использовании шейдерного видео-процессора.
    Signed-off-by: Aleksoid1978 <Aleksoid1978@mail.ru>

--- Source/DX9VideoProcessor.cpp
+++ Source/DX9VideoProcessor.cpp
@@ -1379,9 +1379,9 @@ HRESULT CDX9VideoProcessor::GetAspectRatio(long *plAspectX, long *plAspectY)
 
 HRESULT CDX9VideoProcessor::GetCurentImage(long *pDIBImage)
 {
-	if (m_SrcSamples.Empty()) {
-		return E_FAIL;
-	}
+	//if (m_SrcSamples.Empty()) {
+	//	return E_FAIL;
+	//}
 
 	CRect rSrcRect(m_srcRect);
 	int w = rSrcRect.Width();
