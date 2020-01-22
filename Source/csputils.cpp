658300063d534da34572f8ca6f2bb1949258a9c8
v0lt <mpc-be@mail.ru>
2020-01-22 09:33:18 +0300

    Дополнение к 96cfbe9.

--- Source/csputils.cpp
+++ Source/csputils.cpp
@@ -37,6 +37,20 @@ void mp_invert_matrix3x3(float m[3][3])
     }
 }
 
+// A := A * B
+static void mp_mul_matrix3x3(float a[3][3], float b[3][3])
+{
+    float a00 = a[0][0], a01 = a[0][1], a02 = a[0][2],
+          a10 = a[1][0], a11 = a[1][1], a12 = a[1][2],
+          a20 = a[2][0], a21 = a[2][1], a22 = a[2][2];
+
+    for (int i = 0; i < 3; i++) {
+        a[0][i] = a00 * b[0][i] + a01 * b[1][i] + a02 * b[2][i];
+        a[1][i] = a10 * b[0][i] + a11 * b[1][i] + a12 * b[2][i];
+        a[2][i] = a20 * b[0][i] + a21 * b[1][i] + a22 * b[2][i];
+    }
+}
+
 // Get multiplication factor required if image data is fit within the LSBs of a
 // higher smaller bit depth isfixed-point texture data.
 double mp_get_csp_mul(enum mp_csp csp, int input_bits, int texture_bits)
