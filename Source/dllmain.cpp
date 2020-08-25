797c5e0658d1a8ae32fbb93b8c323525828e4e21
v0lt <mpc-be@mail.ru>
2020-08-25 18:27:04 +0300

    Добавлена DLL-функция OpenConfiguration для вызова настроек из командной строки. Пример вызова: rundll32.exe MpcVideoRenderer64.ax,OpenConfiguration

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -76,3 +76,56 @@ BOOL WINAPI DllMain(HINSTANCE hDllHandle, DWORD dwReason, LPVOID pReserved)
 	}
 	return DllEntryPoint(hDllHandle, dwReason, pReserved);
 }
+
+void CALLBACK OpenConfiguration(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HRESULT hr = S_OK;
+	CUnknown *pInstance = CreateInstance<CMpcVideoRenderer>(nullptr, &hr);
+	IBaseFilter *pFilter = nullptr;
+	pInstance->NonDelegatingQueryInterface(IID_IBaseFilter, (void **)&pFilter);
+	if (pFilter) {
+		pFilter->AddRef();
+
+		CoInitialize(nullptr);
+
+		// Get PropertyPages interface
+		ISpecifyPropertyPages *pProp = nullptr;
+		HRESULT hr = pFilter->QueryInterface<ISpecifyPropertyPages>(&pProp);
+		if (SUCCEEDED(hr) && pProp)
+		{
+			// Get the filter's name and IUnknown pointer.
+			FILTER_INFO FilterInfo;
+			hr = pFilter->QueryFilterInfo(&FilterInfo);
+			// We don't need the graph, so don't sit on a ref to it
+			if (FilterInfo.pGraph)
+				FilterInfo.pGraph->Release();
+
+			IUnknown *pFilterUnk = nullptr;
+			pFilter->QueryInterface<IUnknown>(&pFilterUnk);
+
+			// Show the page.
+			CAUUID caGUID;
+			pProp->GetPages(&caGUID);
+			pProp->Release();
+			hr = OleCreatePropertyFrame(
+				nullptr,            // Parent window
+				0, 0,               // Reserved
+				FilterInfo.achName, // Caption for the dialog box
+				1,                  // Number of objects (just the filter)
+				&pFilterUnk,        // Array of object pointers.
+				caGUID.cElems,      // Number of property pages
+				caGUID.pElems,      // Array of property page CLSIDs
+				0,                  // Locale identifier
+				0, nullptr          // Reserved
+			);
+
+			// Clean up.
+			pFilterUnk->Release();
+			CoTaskMemFree(caGUID.pElems);
+
+			hr = S_OK;
+		}
+		CoUninitialize();
+	}
+	delete pInstance;
+}
