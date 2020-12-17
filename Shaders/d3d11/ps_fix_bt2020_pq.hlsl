25abee8b77085072d87718431eba21d905cd8203
v0lt <mpc-be@mail.ru>
2020-12-17 14:03:58 +0300

    Дополнение для шейдеров "fix_bt2020_pq".

--- Shaders/d3d11/ps_fix_bt2020_pq.hlsl
+++ Shaders/d3d11/ps_fix_bt2020_pq.hlsl
@@ -3,6 +3,15 @@ SamplerState samp : register(s0);
 
 #include "../convert/colorspace_gamut_conversion.hlsl"
 
+static const float ST2084_m1 = 2610.0f / (4096.0f * 4.0f);
+static const float ST2084_m2 = (2523.0f / 4096.0f) * 128.0f;
+static const float ST2084_c1 = 3424.0f / 4096.0f;
+static const float ST2084_c2 = (2413.0f / 4096.0f) * 32.0f;
+static const float ST2084_c3 = (2392.0f / 4096.0f) * 32.0f;
+
+#define SRC_LUMINANCE_PEAK     10000.0
+#define DISPLAY_LUMINANCE_PEAK 125.0
+
 struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
@@ -11,10 +20,25 @@ struct PS_INPUT
 
 float4 main(PS_INPUT input) : SV_Target
 {
-	//TODO
-    float4 color = tex.Sample(samp, input.Tex);
+    //TODO
+    float4 pixel = tex.Sample(samp, input.Tex);
 
-    color.rgb = Colorspace_Gamut_Conversion_2020_to_709(color.rgb);
+    pixel = saturate(pixel); // use saturate(), because pow() can not take negative values
 
-    return color;
+    // ST2084 to Linear
+    pixel.rgb = pow(pixel.rgb, 1.0 / ST2084_m2);
+    pixel.rgb = max(pixel.rgb - ST2084_c1, 0.0) / (ST2084_c2 - ST2084_c3 * pixel.rgb);
+    pixel.rgb = pow(pixel.rgb, 1.0 / ST2084_m1);
+
+    // Peak luminance
+    pixel.rgb = pixel.rgb * (SRC_LUMINANCE_PEAK / DISPLAY_LUMINANCE_PEAK);
+
+    // Colorspace Gamut Conversion
+    pixel.rgb = Colorspace_Gamut_Conversion_2020_to_709(pixel.rgb);
+
+    // Linear to sRGB
+    pixel.rgb = saturate(pixel.rgb);
+    pixel.rgb = pow(pixel.rgb, 1.0 / 2.2);
+
+    return pixel;
 }
