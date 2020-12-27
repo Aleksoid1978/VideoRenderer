efc2d714147bb544491b260e274c9f8e3173ad2d
v0lt <mpc-be@mail.ru>
2020-12-27 21:02:15 +0300

    Добавлен файл conv_matrix.hlsl.

--- Shaders/d3d11/ps_fix_ycgco.hlsl
+++ Shaders/d3d11/ps_fix_ycgco.hlsl
@@ -3,19 +3,7 @@
 Texture2D tex : register(t0);
 SamplerState samp : register(s0);
 
-static float4x4 rgb_ycbcr709 = {
-     0.2126,  0.7152,  0.0722, 0.0,
-    -0.1146, -0.3854,  0.5,    0.0,
-     0.5,    -0.4542, -0.0458, 0.0,
-     0.0,     0.0,     0.0,    0.0
-};
-
-static float4x4 ycgco_rgb = {
-    1.0, -1.0,  1.0, 0.0,
-    1.0,  1.0,  0.0, 0.0,
-    1.0, -1.0, -1.0, 0.0,
-    0.0,  0.0,  0.0, 0.0
-};
+#include "../convert/conv_matrix.hlsl"
 
 static const float4x4 rgb_ycbcr709_ycgco_rgb = mul(ycgco_rgb, rgb_ycbcr709);
 
