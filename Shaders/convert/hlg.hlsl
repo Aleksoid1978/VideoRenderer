bff7d77a4e9331790f90889d2d1bbd72d20eedaf
v0lt <mpc-be@mail.ru>
2020-12-30 09:01:47 +0300

    Shaders: дополнение.

--- Shaders/convert/hlg.hlsl
+++ Shaders/convert/hlg.hlsl
@@ -1,12 +1,11 @@
-inline float3 inverse_HLG(float3 x)
+inline float3 inverse_HLG(float3 rgb)
 {
     const float B67_a = 0.17883277;
     const float B67_b = 0.28466892;
     const float B67_c = 0.55991073;
     const float B67_inv_r2 = 4.0;
-    x = (x <= 0.5)
-        ? x * x * B67_inv_r2
-        : exp((x - B67_c) / B67_a) + B67_b;
-
-    return x;
+    rgb = (rgb <= 0.5)
+        ? rgb * rgb * B67_inv_r2
+        : exp((rgb - B67_c) / B67_a) + B67_b;
+    return rgb;
 }
--- Shaders/convert/st2084.hlsl
+++ Shaders/convert/st2084.hlsl
@@ -3,3 +3,21 @@ static const float ST2084_m2 = (2523.0f / 4096.0f) * 128.0f;
 static const float ST2084_c1 =  3424.0f / 4096.0f;
 static const float ST2084_c2 = (2413.0f / 4096.0f) * 32.0f;
 static const float ST2084_c3 = (2392.0f / 4096.0f) * 32.0f;
+
+inline float4 ST2084ToLinear(float4 rgb, float factor)
+{
+    rgb = pow(rgb, 1.0 / ST2084_m2);
+    rgb = max(rgb - ST2084_c1, 0.0) / (ST2084_c2 - ST2084_c3 * rgb);
+    rgb = pow(rgb, 1.0 / ST2084_m1);
+    rgb *= factor;
+    return rgb;
+}
+
+inline float4 LinearToST2084(float4 rgb, float divider)
+{
+    rgb /= divider;
+    rgb = pow(rgb, ST2084_m1);
+    rgb = (ST2084_c1 + ST2084_c2 * rgb) / (1.0f + ST2084_c3 * rgb);
+    rgb = pow(rgb, ST2084_m2);
+    return rgb;
+}
