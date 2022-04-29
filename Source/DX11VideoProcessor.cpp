4504b3c4618e19e67681c9c7a9325b92d9be0726
v0lt <mpc-be@mail.ru>
2022-04-29 13:05:59 +0300

    USE_D3D11_SUBPIC: Для ресайза субтитров используем билинейную интерполяцию.

--- Source/DX11VideoProcessor.cpp
+++ Source/DX11VideoProcessor.cpp
@@ -2084,7 +2084,7 @@ HRESULT CDX11VideoProcessor::Render(int field)
 			m_pDeviceContext->OMSetBlendState(m_pFilter->m_bSubInvAlpha ? m_pAlphaBlendStateInv : m_pAlphaBlendState, nullptr, D3D11_DEFAULT_SAMPLE_MASK);
 			m_pDeviceContext->VSSetShader(m_pVS_Simple, nullptr, 0);
 			m_pDeviceContext->PSSetShader(m_pPS_Simple, nullptr, 0);
-			m_pDeviceContext->PSSetSamplers(0, 1, &m_pSamplerPoint);
+			m_pDeviceContext->PSSetSamplers(0, 1, &m_pSamplerLinear);
 			m_pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
 			//everything is set directly on the renderer so we dont have to compile any shaders on the player side
