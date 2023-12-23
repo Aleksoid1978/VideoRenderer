25ade1fe129e3da5a6eb61260a4c3a34c4bbc5f9
v0lt <mpc-be@mail.ru>
2023-12-23 08:13:36 +0300

    В update_submodules.cmd добавлено больше информации.

--- update_submodules.cmd
+++ update_submodules.cmd
@@ -8,12 +8,20 @@ IF /I %ERRORLEVEL%==0 GOTO :GitOK
 
 SET gitexe="c:\Program Files\Git\cmd\git.exe"
 IF NOT EXIST %gitexe% set gitexe="c:\Program Files\Git\bin\git.exe"
-IF NOT EXIST %gitexe% GOTO :END
+IF NOT EXIST %gitexe% (
+  ECHO ERROR: Git not found!
+  GOTO :END
+)
 
 :GitOK
 
-%gitexe% submodule update --init --recursive
-
+%gitexe% submodule update --init --recursive --progress
+IF %ERRORLEVEL%==0 (
+   ECHO Submodule update completed successfully.
+) ELSE (
+   ECHO ERROR: %errorlevel%!
+)
 :END
+TIMEOUT /T 3
 ENDLOCAL
 EXIT /B
