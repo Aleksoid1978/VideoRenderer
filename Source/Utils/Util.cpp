f3b67030bff59817e2a399833e93bc1b814575af
v0lt <mpc-be@mail.ru>
2024-09-15 20:15:02 +0300

    Utils: Косметика кода.

--- Source/Utils/Util.cpp
+++ Source/Utils/Util.cpp
@@ -24,17 +24,17 @@
 VERSIONHELPERAPI
 IsWindows11OrGreater() // https://walbourn.github.io/windows-sdk-for-windows-11/
 {
-	OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0, {0}, 0, 0 };
+	OSVERSIONINFOEXW osvi = {
+		.dwOSVersionInfoSize = sizeof(osvi),
+		.dwMajorVersion = HIBYTE(_WIN32_WINNT_WIN10),
+		.dwMinorVersion = LOBYTE(_WIN32_WINNT_WIN10),
+		.dwBuildNumber = 22000, };
 	DWORDLONG const dwlConditionMask = VerSetConditionMask(
 		VerSetConditionMask(
 		VerSetConditionMask(
 			0, VER_MAJORVERSION, VER_GREATER_EQUAL),
 			   VER_MINORVERSION, VER_GREATER_EQUAL),
-			   VER_BUILDNUMBER, VER_GREATER_EQUAL);
-
-	osvi.dwMajorVersion = HIBYTE(_WIN32_WINNT_WIN10);
-	osvi.dwMinorVersion = LOBYTE(_WIN32_WINNT_WIN10);
-	osvi.dwBuildNumber = 22000;
+			   VER_BUILDNUMBER,  VER_GREATER_EQUAL);
 
 	return VerifyVersionInfoW(&osvi, VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER, dwlConditionMask) != FALSE;
 }
@@ -68,16 +68,16 @@ LPCWSTR GetWinVer()
 		{ 10240, L"10 1507" },
 	};
 
-	OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0, {0}, 0, 0 };
+	OSVERSIONINFOEXW osvi = {
+		.dwOSVersionInfoSize = sizeof(osvi),
+		.dwMajorVersion = HIBYTE(_WIN32_WINNT_WIN10),
+		.dwMinorVersion = LOBYTE(_WIN32_WINNT_WIN10), };
 	DWORDLONG const dwlConditionMask = VerSetConditionMask(
 		VerSetConditionMask(
-			VerSetConditionMask(
-				0, VER_MAJORVERSION, VER_GREATER_EQUAL),
-			VER_MINORVERSION, VER_GREATER_EQUAL),
-		VER_BUILDNUMBER, VER_GREATER_EQUAL);
-
-	osvi.dwMajorVersion = HIBYTE(_WIN32_WINNT_WIN10);
-	osvi.dwMinorVersion = LOBYTE(_WIN32_WINNT_WIN10);
+		VerSetConditionMask(
+			0, VER_MAJORVERSION, VER_GREATER_EQUAL),
+			   VER_MINORVERSION, VER_GREATER_EQUAL),
+			   VER_BUILDNUMBER,  VER_GREATER_EQUAL);
 
 	for (const auto& win10ver : win10versions) {
 		osvi.dwBuildNumber = win10ver.buildNumber;
