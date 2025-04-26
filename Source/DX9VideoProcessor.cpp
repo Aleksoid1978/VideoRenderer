e07dd13ea55e28e779e35db778649063b92d1f0e
v0lt <mpc-be@mail.ru>
2025-04-26 12:00:46 +0300

    Исправлено копирование данные неправильного размера, которые могут получаться из кривых файлов AVI, которые может делать FFmpeg. Пример: ffmpeg -i "input.avi" -c:v libx265 output.avi В этом случае будет получен файл AVI с нулевым полем FOURCC, что соответствует BI_RGB. Но сами данные будут занимать меньше места, что вызовет падение видеорендерера.

--- Source/DX9VideoProcessor.cpp
+++ Source/DX9VideoProcessor.cpp
@@ -1469,7 +1469,7 @@ HRESULT CDX9VideoProcessor::CopySample(IMediaSample* pSample)
 
 		BYTE* data = nullptr;
 		const long size = pSample->GetActualDataLength();
-		if (size > 0 && S_OK == pSample->GetPointer(&data)) {
+		if (size >= abs(m_srcPitch) * m_srcLines && S_OK == pSample->GetPointer(&data)) {
 			if (m_srcParams.cformat == CF_NONE) {
 				return E_FAIL;
 			}
