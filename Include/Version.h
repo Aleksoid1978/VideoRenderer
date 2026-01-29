7447615f51a4c234c1a7c91d1d7a9915198fb11c
v0lt <mpc-be@mail.ru>
2026-01-29 20:26:01 +0300

    Смена версии на 0.9.20 dev.

--- Include/Version.h
+++ Include/Version.h
@@ -21,11 +21,11 @@
 #define DO_MAKE_STR(x) #x
 #define MAKE_STR(x)    DO_MAKE_STR(x)
 
-#define VER_RELEASE    1
+#define VER_RELEASE    0
 
 #define VER_MAJOR      0
 #define VER_MINOR      9
-#define VER_BUILD      19
+#define VER_BUILD      20
 
 #define VERSION_NUM    VER_MAJOR,VER_MINOR,VER_BUILD,REV_NUM
 #define VERSION_STR    MAKE_STR(VER_MAJOR) "." MAKE_STR(VER_MINOR) "." MAKE_STR(VER_BUILD) "." MAKE_STR(REV_NUM)
